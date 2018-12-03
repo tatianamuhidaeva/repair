b60414a0f30cc43e3a96f2e504884d34bbacea08
Татьяна Мухидаева <batagaeva.ta@gmail.com>
2018-12-03 13:47:30 +0800

    Исправлена ошибка отправки пустой формы, изменен адаптив Заявки, изменен адаптив сообщения об успешной отправки

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-
-  // });
+  $('button.btn-submit').prop('disabled', 'disabled');
+  
   /* Валидация форм v*/
   $.validator.addMethod("letters", function (value, element) {
     return this.optional(element) || value == value.match(/^[a-zA-ZА-Яа-я\s]*$/);
   });
   $.validator.addMethod("isPhone", function (value, element) {
-    return this.optional(element) || value == value.match(/\+7\-[0-9]{3}\-[0-9]{2}\-[0-9]{2}\-[0-9]{3}/);
+    return this.optional(element) || value == value.match(/\+7[0-9]{10}/);
   });
   $successMsg = $(".messageShow");
 
@@ -24,11 +24,8 @@ $(document).ready(function () {
     },
     messages: {
       user_name: "Введите Ваше имя (Более 2 букв, рус en, пробел)",
-      user_phone: "Введите номер в формате +7-999-99-99-999"
+      user_phone: "Введите номер в формате +79998877666"
     },
-    submitHandler: function () {
-      $successMsg.show();
-    }
   });
   $("#form-2").validate({
     rules: {
@@ -44,10 +41,7 @@ $(document).ready(function () {
     },
     messages: {
       user_name: "Введите Ваше имя (Более 2 букв, рус en, пробел)",
-      user_phone: "Введите номер в формате +7-999-99-99-999"
-    },
-    submitHandler: function () {
-      $successMsg.show();
+      user_phone: "Введите номер в формате +79998877666"
     }
   });
   $("#form-3").validate({
@@ -64,29 +58,52 @@ $(document).ready(function () {
     },
     messages: {
       user_name: "Введите Ваше имя (Более 2 букв, рус en, пробел)",
-      user_phone: "Введите номер в формате +7-999-99-99-999"
-    },
-    // submitHandler: function () {
-    //   $successMsg.show();
-    // }
+      user_phone: "Введите номер в формате +79998877666"
+    }
   });
-
-
-  /* Обработчик форм. Посылает письмо на почту*/
+  $('#form-1 input').on('keyup blur', function () { // fires on every keyup & blur
+    if ($('#form-1').valid()) {
+       // checks form for validity
+      $('#form-1 > .btn-submit').prop('disabled', false); // enables button
+    } else {
+      
+      $('#form-1 > .btn-submit').prop('disabled', 'disabled'); // disables button
+    }
+  });
+  $('#form-2 input').on('keyup blur', function () { // fires on every keyup & blur
+    if ($('#form-2').valid()) {
+       // checks form for validity
+      $('#form-2 > .btn-submit').prop('disabled', false); // enables button
+    } else {
+      
+      $('#form-2 > .btn-submit').prop('disabled', 'disabled'); // disables button
+    }
+  });
+  $('#form-3 input').on('keyup blur', function () { // fires on every keyup & blur
+    if ($('#form-3').valid()) {
+       // checks form for validity
+      $('#form-3 > .btn-submit').prop('disabled', false); // enables button
+    } else {
+      
+      $('#form-3 > .btn-submit').prop('disabled', 'disabled'); // disables button
+    }
+  });
   $('.ajax-submit').on('submit', function (event) {
+    // if ($('#form-1').valid()) {
     event.preventDefault();
     $.ajax({
       type: "POST",
       url: "./mailer/smart.php",
       data: $(this).serialize()
     }).done(function () {
-      $(this).find("input").val("");
+      $(this).find(".ajax-submit input").val("");
       $('#modal-success').toggleClass('flex');
       $(".ajax-submit").trigger("reset");
+      $('.ajax-submit .btn-submit').prop('disabled', 'disabled');
     });
     return false;
+    // }
   });
-
 
   /* Вызов модального окна */
   $('#btn-callback-1').on('click', function () {
@@ -98,10 +115,13 @@ $(document).ready(function () {
   $('.modal__close').on('click', function () {
     $('.modal').toggleClass('flex');
   });
-  $('#modal-success').on('click', function () {
-    $('.modal-success').toggleClass('flex');
+  $('.modal-success__close').on('click', function () {
+    $('#modal-success').toggleClass('flex');
   });
-
+  $('.btn-success__close').on('click', function () {
+    $('#modal-success').toggleClass('flex');
+  });
+  
   /* Portfolio Slick. Слайдер в блоке Портфолио */
   $('.portfolio-slider').slick({
     infinite: true,
